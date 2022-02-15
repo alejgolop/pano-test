45bc1237f2bf7e8256566a9e56645306b2c80c24
Alejandro Gómez <alejgolop@gmail.com>
2022-02-15 12:28:40 +0100

    Initial commit

---
+++ main_linked.js
@@ -0,0 +1,49 @@
+
+
+var origin="https://sfield.s3.eu-central-1.amazonaws.com/pano/";
+
+var panorama6 = new PANOLENS.ImagePanorama( origin+"6.jpg" );
+var panorama7 = new PANOLENS.ImagePanorama( origin+"7.jpg" );
+var panorama9 = new PANOLENS.ImagePanorama( origin+"9.jpg" );
+var panorama3 = new PANOLENS.ImagePanorama( origin+"3.jpg" );
+
+var viewer = new PANOLENS.Viewer( { output: 'console' } );
+ viewer.add( panorama6 );
+ viewer.add( panorama7 );
+ viewer.add( panorama9 );
+ viewer.add( panorama3 );
+
+//Links
+linkPanoramas(panorama6, panorama7,new THREE.Vector3( -4106.08, 56.71, -2840.92 ));
+linkPanoramas(panorama7, panorama6, new THREE.Vector3( 3016.80, -375.40, 3963.07 ));
+linkPanoramas(panorama6, panorama9, new THREE.Vector3( -318.13, -406.98, 4966.18 ));
+
+linkPanoramas(panorama9, panorama6, new THREE.Vector3( 1019.15, -769.32, -4833.27 ));
+linkPanoramas(panorama9, panorama3, new THREE.Vector3( -4905.57, -646.52, -655.41 ));
+linkPanoramas(panorama3, panorama9, new THREE.Vector3( 4035.40, -446.50, 2913.81 ));
+
+
+  //de 7 a 6
+  //3016.80, -375.40, 3963.07
+
+  //de 6 a 7
+  //-4106.08, 56.71, -2840.92
+
+  //de 6 a 9
+  //-318.13, -406.98, 4966.18
+
+  //de 9 a 6
+  //1019.15, -769.32, -4833.27
+
+  //de 9 a 3
+  //-4905.57, -646.52, -655.41
+
+  //de 3 a 9 
+  //4035.40, -446.50, 2913.81
+
+//InfoSpots
+
+
+panorama6.add(createInfoSpot(new THREE.Vector3( -4029.06, -1151.02, -2716.20 ), 'Una Piscina'));
+
+
---
+++ template.js
@@ -0,0 +1,60 @@
+ /*
+  Instrucciones:
+  -Subir panoramas .jpg a S3. Para pruebas usamos el bucket sfield/pano. Al subir, en permisos marcar "Conceder acceso de lectura público" y cargar
+  -Duplicar este archivo 'template.js' para una nueva ruta/proyecto y renombrar
+  -Cambiar main.js de index.html con el nombre del nuevo archivo.js, que es donde vamos a trabajar
+  -Definir todos los objetos panorama => variable origin + el nombre del archivo
+  -Cargarlos en el Viewer
+  -En Google Chrome, abrir consola con 'Ctrl+Shift+I' abrir la consola
+  -Ahora al hacer 'Ctrl+Click' Izquierdo sobre el panorama aparecerán las coordenadas
+  -Incluir esas coordenadas en el link entre panoramas para que aparezca el icono
+  -No olvidar el link de vuelta desde el manorama destino al origen para no 'encerrar' al usuario
+  
+  */
+
+
+
+
+var origin="https://sfield.s3.eu-central-1.amazonaws.com/pano/";
+
+var panorama6 = new PANOLENS.ImagePanorama( origin+"6.jpg" );
+var panorama7 = new PANOLENS.ImagePanorama( origin+"7.jpg" );
+
+
+var viewer = new PANOLENS.Viewer( { output: 'console' } );
+ viewer.add( panorama6 );
+ viewer.add( panorama7 );
+
+
+//Links
+linkPanoramas(panorama6, panorama7,new THREE.Vector3( -4106.08, 56.71, -2840.92 ));
+linkPanoramas(panorama7, panorama6, new THREE.Vector3( 3016.80, -375.40, 3963.07 ));
+
+
+//InfoSpots
+panorama6.add(createInfoSpot(new THREE.Vector3( -4029.06, -1151.02, -2716.20 ), 'Una Piscina'));
+
+
+  //de 7 a 6
+  //3016.80, -375.40, 3963.07
+
+  //de 6 a 7
+  //-4106.08, 56.71, -2840.92
+
+
+
+
+  /*
+  Instrucciones:
+  -Subir panoramas .jpg a S3. Para pruebas usamos el bucket sfield/pano. Al subir, en permisos marcar "Conceder acceso de lectura público" y cargar
+  -Duplicar template.js para una nueva ruta/proyecto
+  -Cambiar main.js de index.html con el nombre del nuevo .js
+  -Definir todos los objetos panorama => variable origin + el nombre del archivo
+  -Cargarlos en el Viewer
+  -En Google Chrome, abrir consola con 'Ctrl+Shift+I' abrir la consola
+  -Ahora al hacer 'Ctrl+Click' Izquierdo sobre el panorama aparecerán las coordenadas
+  -Incluir esas coordenadas en el link entre panoramas para que aparezca el icono
+  -No olvidar el link de vuelta desde el panorama destino al origen para no 'encerrar' al usuario
+  -Para los InfoSpots sustituir el panorama objetivo y las coordenadas como anteriormente
+  
+  */
