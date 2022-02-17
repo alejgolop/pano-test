eaf370d677de4fb3e192d5a590acd78fefd9238a
Alejandro Gómez <alejgolop@gmail.com>
2022-02-17 12:48:47 +0100

    better instructions

--- template.js
+++ template.js
@@ -10,24 +10,24 @@
   -Incluir esas coordenadas en el link entre panoramas para que aparezca el icono
   -No olvidar el link de vuelta desde el panorama destino al origen para no 'encerrar' al usuario
   -Para los InfoSpots sustituir el panorama objetivo, las coordenadas como anteriormente y personalizar el texto
-  -Se pueden definir puntos de vista inicial por cada panorama (sólo efectivos desde navegador web, no móvil)
+  -Se pueden definir puntos de vista inicial para cada panorama (sólo efectivos desde navegador web, no móvil)
   */
 
 
 var origin="https://sfield.s3.eu-central-1.amazonaws.com/pano/";
 
-//Aquí van los Panoramas
+//Aquí van los Panoramas.
 var panorama6 = createImagePanorama(origin+"6.jpg");
 var panorama7 = createImagePanorama(origin+"7.jpg");
 
 
-//Aquí cargamos los Panoramas en el Viewer
+//Aquí cargamos los Panoramas en el Viewer.
 var viewer = new PANOLENS.Viewer( { output: 'console' } );
  viewer.add( panorama6 );
  viewer.add( panorama7 );
 
 
-//Si es un móvil, activa el control de los sensores por defecto
+//Si es un móvil, activa el control de los sensores por defecto.
  if(isMobile())
  {
    viewer.enableControl(PANOLENS.CONTROLS.DEVICEORIENTATION);
@@ -42,6 +42,6 @@ linkPanoramas(panorama7, panorama6, new THREE.Vector3( 3016.80, -375.40, 3963.07
 panorama6.add(createInfoSpot(new THREE.Vector3( -4029.06, -1151.02, -2716.20 ), 'Una Piscina'));
 
 
-//Aquí definimos el punto de vista inicial de los panoramas, el cual no es obligatorio, sólo funciona en navegador web, no en el móvil (por el control basado en sensores)
+//Aquí definimos el punto de vista inicial de cada panorama, el cual no es obligatorio. Sólo funciona en navegador web, no en el móvil (por el control basado en sensores).
 initialLookAt(panorama6,new THREE.Vector3(-4247.17, -590.58, -2556.46));
  
